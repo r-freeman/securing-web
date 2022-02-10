0a30c76520fd67fcf03c3327f9966587ff738cf5
Ryan Freeman <ryan.freeman@aerlingus.com>
2022-02-10 10:00:05 +0000

    Updated WebSecurityConfig class

--- src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -1,2 +1,25 @@
-package com.example.securingweb;public class WebSecurityConfig {
+package com.example.securingweb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("user").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
